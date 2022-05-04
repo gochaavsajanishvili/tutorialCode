7a17a2f2bacea54786784b04d34ccabc32f5aeec
gochaavsajanishvili <ageperbg@gmail.com>
2022-05-04 09:25:50 +0400

    In previous lesson we worked on CREATE from CRUD and now we added the READ functionality

--- Fullstack Javascrpit From Scratch by Brad Schiff/todo-app/server.js
+++ Fullstack Javascrpit From Scratch by Brad Schiff/todo-app/server.js
@@ -55,57 +55,60 @@ app.use(express.urlencoded({extended: false}))
 
 app.get('/', function(req, res) {
   // Brad promised that we won't do this anymore :d
-  res.send(`<!DOCTYPE html>
-  <html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple To-Do App</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container">
-      <h1 class="display-4 text-center py-1">To-Do App</h1>
-      
-      <div class="jumbotron p-3 shadow-sm">
-        <!-- Here we go, we specified url for form to be submitted which we just wrote logic for what to do after submition -->
-        <!-- And we added post req so it gonna trigger the post method in js -->
-        <form action="/create-item" method="POST">
-          <div class="d-flex align-items-center">
-            <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
-            <button class="btn btn-primary">Add New Item</button>
-          </div>
-        </form>
+  // We don't want to display/send this html as soon as user sends request anymore, we now want to connect to database before it
+  // So we could read data from and for further display to user
+  // Here we access the database collection and use .find() method on it to READ from it
+  // In .find() method we can send query as an argument to filter out the data, intuitically, to find or READ all data, we leave paranthases blank
+  // .find() method is gonna return data in a format which is hard to read for people and is not easy to work with javascript, so we use
+  // .toArray() method to convert that data into super easy to work with javascript array
+  // .toArray() expects function as an argument, which will be called only after the database operation before it is complete
+  // the items param will get the array value
+  db.collection('items').find().toArray(function(err, items) {
+    // Brad is big fan of visualazing data before making next move :d
+    // console.log(items)
+    // We moved up the html code into this function, because we don't want to page display before the above database function has had a chance to complete
+    res.send(`<!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Simple To-Do App</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    </head>
+    <body>
+      <div class="container">
+        <h1 class="display-4 text-center py-1">To-Do App</h1>
+        
+        <div class="jumbotron p-3 shadow-sm">
+          <!-- Here we go, we specified url for form to be submitted which we just wrote logic for what to do after submition -->
+          <!-- And we added post req so it gonna trigger the post method in js -->
+          <form action="/create-item" method="POST">
+            <div class="d-flex align-items-center">
+              <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+              <button class="btn btn-primary">Add New Item</button>
+            </div>
+          </form>
+        </div>
+        
+        <ul class="list-group pb-5">
+        <!-- Here we added the join method, which converts array into string of text and in its arg we can choose what will seperate each array item -->
+        <!-- In this case we chose nothing -->
+          ${items.map(function(item) {
+            return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+            <span class="item-text">${item.text}</span>
+            <div>
+              <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+              <button class="delete-me btn btn-danger btn-sm">Delete</button>
+            </div>
+          </li>`
+          }).join('')}
+        </ul>
+        
       </div>
       
-      <ul class="list-group pb-5">
-        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-          <span class="item-text">Fake example item #1</span>
-          <div>
-            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-            <button class="delete-me btn btn-danger btn-sm">Delete</button>
-          </div>
-        </li>
-        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-          <span class="item-text">Fake example item #2</span>
-          <div>
-            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-            <button class="delete-me btn btn-danger btn-sm">Delete</button>
-          </div>
-        </li>
-        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-          <span class="item-text">Fake example item #3</span>
-          <div>
-            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-            <button class="delete-me btn btn-danger btn-sm">Delete</button>
-          </div>
-        </li>
-      </ul>
-      
-    </div>
-    
-  </body>
-  </html>`)
+    </body>
+    </html>`)
+  })
 })
 
 // So again, to receive user data, we use this method
@@ -129,7 +132,10 @@ app.post('/create-item', function(req, res) {
   db.collection('items').insertOne({text: req.body.item}, function() {
     // This line was written outside of this function and we moved it up because we didn't want it to display asap, we wanted it to
     // Display after the document is created in the db
-    res.send("Thanks for submitting the form.")
+    // We changed this send line to redirect because after submitting the form we want to automatically go back homepage and see the result immediately
+    // res.send("Thanks for submitting the form.")
+    // Here in args we specify where we want our user to redirect, in this case we said we want to homepage, that slash means that
+    res.redirect('/')
   })
 })
 
